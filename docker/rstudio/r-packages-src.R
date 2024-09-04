f2b3910cc7906349240a8dc326955051dc0e993e
ssbsjv <156660008+ssbsjv@users.noreply.github.com>
2024-09-04 10:59:51 +0200

    Update r-packages-src.R

--- docker/rstudio/r-packages-src.R
+++ docker/rstudio/r-packages-src.R
@@ -11,3 +11,5 @@ install.packages(pkgs, dependencies=TRUE, repos='https://packagemanager.rstudio.
 
 install.packages('/tmp/ROracle_1.4-1_R_x86_64-unknown-linux-gnu.tar.gz', repos = NULL, type='source')
 
+install.packages('arrow', dependencies=FALSE, repos='https://packagemanager.rstudio.com/cran/latest')
+
