c0c6d8b43ebbe9e4b48fcc7b643adb424414fe90
ssbsjv <156660008+ssbsjv@users.noreply.github.com>
2024-09-04 08:52:47 +0200

    Update r-packages-src.R

--- docker/rstudio/r-packages-src.R
+++ docker/rstudio/r-packages-src.R
@@ -1,7 +1,13 @@
 pkgs <- c(
-    'DBI',
-  'leaflet'
+    'DBI','leaflet','getPass', 'rJava','rjwsacruncher','sfarrow','Rcurl', 'esquisse','dcmodify','googleCloudStorageR','tidyfst','rJava', 'Hmisc','sdcTable', 'DT', 'configr','dggridR',
+    'SmallCountRounding',
+    'PxWebApiData',
+    'SSBtools',
+    'klassR',
+    'GISSB',
+    'GaussSuppression'
 )
-
 install.packages(pkgs, dependencies=TRUE, repos='https://packagemanager.rstudio.com/cran/latest')
+
+install.packages('/tmp/ROracle_1.4-1_R_x86_64-unknown-linux-gnu.tar.gz', repos = NULL, type='source')
 
